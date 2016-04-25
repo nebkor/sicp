5938d12bc367ec2ffeb0bb5ce3be9c60b4885ea9
Joe Ardent <code@ardent.nebcorp.com>
2016-04-25 18:45:46 -0400

    2.68 done

---
+++ 2.3.4.rkt
@@ -0,0 +1,122 @@
+#lang racket
+
+;; helper
+(define (member? val lst)
+  (not (equal? #f (member val lst))))
+
+;;;;;;;;;;;;;;;;;;;
+;; Section 2.3.4: Huffman encoding trees
+;;;;;;;;;;;;;;;;;;;
+
+;; given:
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left)
+                (symbols right))
+        (+ (weight left) (weight right))))
+
+(define (left-branch tree) (car tree))
+(define (right-branch tree) (cadr tree))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch
+               (choose-branch
+                (car bits)
+                current-branch)))
+          (if (leaf? next-branch)
+              (cons
+               (symbol-leaf next-branch)
+               (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits)
+                        next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "bad bit:
+               CHOOSE-BRANCH" bit))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set)))
+         (cons x set))
+        (else
+         (cons (car set)
+               (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+        (adjoin-set
+         (make-leaf (car pair)    ; symbol
+                    (cadr pair))  ; frequency
+         (make-leaf-set (cdr pairs))))))
+
+;; Exercise 2.67: Define an encoding tree and a sample message:
+
+;; (define sample-tree
+;;   (make-code-tree
+;;    (make-leaf 'A 4)
+;;    (make-code-tree
+;;     (make-leaf 'B 2)
+;;     (make-code-tree
+;;      (make-leaf 'D 1)
+;;      (make-leaf 'C 1)))))
+
+;; (define sample-message
+;;   '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+;; Use the decode procedure to decode the message, and give the result.
+
+;; racket@2.3.4.rkt> (decode sample-message sample-tree)
+;; '(A D A B B C A)
+
+;; given:
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append
+       (encode-symbol (car message) tree)
+       (encode (cdr message) tree))))
+
+;; 2.68: implement encode-symbol, use the given sample-message and sample-tree
+;; to test the finished encode function.
+(define (encode-symbol sym tree)
+  (define (iter t res)
+    (if (leaf? t)
+        (reverse res)
+        (let ([left (left-branch t)]
+              [right (right-branch t)])
+          (if (member? sym (symbols left))
+              (iter left (cons 0 res))
+              (iter right (cons 1 res))))))
+  (if (not (member? sym (symbols tree)))
+      (error (~a "Symbol '" sym "' is not in the given tree."))
+      (iter tree '())))
+
+;; racket@2.3.4.rkt> (equal? sample-message
+;;                           (encode (decode sample-message sample-tree) sample-tree))
+;; #t
