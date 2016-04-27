b112ace494fe257c22c8572f788da05dfd333b9d
Joe Ardent <code@ardent.nebcorp.com>
2016-04-27 18:07:48 -0400

    Done with 3.5.

--- 3.1.rkt
+++ 3.1.rkt
@@ -46,3 +46,31 @@
 
 (define (call-the-cops)
   (displayln "COPS ARE COMING"))
+
+;; 3.5: use the provided monte-carlo procedure to do numerical integration
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond ((= trials-remaining 0)
+           (/ trials-passed trials))
+          ((experiment)
+           (iter (- trials-remaining 1)
+                 (+ trials-passed 1)))
+          (else
+           (iter (- trials-remaining 1)
+                 trials-passed))))
+  (iter trials 0))
+
+(define (random-in-range low high)
+  (let ([range (- high low)]
+        [r (random)])
+    (+ low (* r range))))
+
+(define (get-area lower-x lower-y upper-x upper-y)
+  (* (- upper-x lower-x)
+     (- upper-y lower-y)))
+
+(define (estimate-integral pred lower-x lower-y upper-x upper-y trials)
+  (let ([area (get-area lower-x lower-y upper-x upper-y)]
+        [experiment (λ () (pred (random-in-range lower-x upper-x)
+                                (random-in-range lower-y upper-y)))])
+    (* area (monte-carlo trials experiment))))
