6f6b8cc77bb3cf038298a5600de12cdb51f9c6c6
Joe Ardent <code@ardent.nebcorp.com>
2016-04-27 18:45:59 -0400

    don't create an explicit binding for random call

--- 3.1.rkt
+++ 3.1.rkt
@@ -61,9 +61,8 @@
   (iter trials 0))
 
 (define (random-in-range low high)
-  (let ([range (- high low)]
-        [r (random)])
-    (+ low (* r range))))
+  (let ([range (- high low)])
+    (+ low (* (random) range))))
 
 (define (get-area lower-x lower-y upper-x upper-y)
   (* (- upper-x lower-x)
